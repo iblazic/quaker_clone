1a3fce2c1476fd107d611b29fbdb2d7325265d00
iblazic <blazic.ines@gmail.com>
2017-05-08 13:30:11 +0200

    login, registracija

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,12 +5,37 @@
 @endsection
 @section('content')
     <div class="row">
-        <div class="col-6">
+        <div class="col-md-6">
+            <h3>Sign up</h3>
             <form action="#" method="post">
                 <div class="form-group">
                     <label for="email">Your E-Mail</label>
-                    <input class="form-control">
+                    <input class="form-control" type="text" name="email" id="email">
                 </div>
+                <div class="form-group">
+                    <label for="first_name">Your First name</label>
+                    <input class="form-control" type="text" name="first_name" id="first_name">
+                </div>
+                <div class="form-group">
+                    <label for="password">Your password</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+        <div class="col-md-6">
+            <h3>Sign in</h3>
+            <form action="#" method="post">
+                <div class="form-group">
+                    <label for="email">Your E-Mail</label>
+                    <input class="form-control" type="text" name="email" id="email">
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Your password</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
     </div>
